d32726bd2679970a1d3036c17859e7d8188f94d9
Flower Db <dearestdyw@gmail.com>
2018-04-10 14:02:34 +0800

    将 config.js 忽略，不会再上传账号密码到git上。

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+const externalConfig = require('./external-config')
+
 module.exports = {
-  token: '', // 留空不检查token
+  ...externalConfig,
   pocket48: {
     account: '', // 口袋48账号
     password: '', // 口袋48密码
   },
-  IMEI: '866716037125816', // 用于发请求的IMEI码, 15位, 请不要使用自己手机的IMEI以防被封设备
 }
---
+++ external-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  IMEI: '866716037125816', // 用于发请求的IMEI码, 15位, 请不要使用自己手机的IMEI以防被封设备
+  token: '', // 留空不检查token
+}
